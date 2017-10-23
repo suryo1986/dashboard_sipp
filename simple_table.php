9c4de598d28649c0a5e9a48f5eef7d78105a3f3a
suryo1986 <tyo_waskito@yahoo.com>
2017-10-23 23:40:47 +0700

    Add files via upload

---
+++ simple_table.php
@@ -0,0 +1,22 @@
+    <div class="clearfix">	
+		
+	</div>	
+	<div class="row">
+			<div class="col-lg-12">
+				<div class="panel-group">
+				<div class="panel panel-success">
+					<div class="panel-heading">
+					 <h3 class="panel-title pull-left">List Perkara <?php echo $text_filter; ?></h3>
+					 <div class="clearfix"></div>
+					 </div>
+					<div class="panel-body">
+					 <h4 class="panel-title pull-left"><?php echo $text_nama; ?></h4>
+                     <?php echo $html_table;?>
+				
+					</div>
+				</div>		
+				</div>
+			</div>	
+		</div>	
+
+	
